c2f6a4ae806eba37f13ddfd9787bfdca56632c64
rajiss-ctrl <omosanjos77@gmail.com>
2025-06-03 11:47:56 -0700

    implement mobile responsiveness for hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <header className='bg-[url("/pattern.svg")] relative z-40 overflow-hidden bg-cover bg-center bg-no-repeat bg-[rgba(0,0,0,1)] h-[690px] px-[100px] py-[33px]'>
+    <header className='bg-[url("/pattern.svg")] relative z-40 overflow-hidden bg-cover bg-center bg-no-repeat bg-[rgba(0,0,0,1)] h-[533px] lg:h-[690px] px-4 py-6 lg:px-[100px] lg:py-[33px]'>
         <Image src='/background_net.svg' fill alt='background' className='absolute z-0 inzet-10 object-contain'/>
-        <Image src='/globe.svg' width={600} height={600} alt='background' className='spin-globe absolute top-[120px] -right-80 z-10 '/>
+        <Image src='/globe.svg' width={600} height={600} alt='background' className='spin-globe absolute top-[120px] -right-80 z-10 hidden lg:block '/>
+        <Image src='/globe.svg' width={600} height={600} alt='background' className='spin-globe absolute top-[120px] -right-60 z-10 lg:hidden'/>
         <Navbar/>
         <div className="relative text-[rgba(255,255,255,1)] z-50 mt-[110px] lg:w-[900px]">
           <div className="light_blue_floating absolute w-[100px] h-[30px] bg-[rgba(71,111,255,1)] rotate-[17.14deg] -left-3 -top-6 text-[10px] pt-[6px] text-center pl-2 rounded-[20px]">Web Development</div>
@@ -15,12 +16,13 @@ const Hero = () => {
           <div className="pink_floating absolute w-[100px] h-[30px] bg-[rgba(255,20,203,1)] right-28 bottom-[54.6667%] text-[10px] pt-[6px] text-center pl-2 rounded-[20px]">Graphic Design</div>
           <div className="orange_floating absolute w-[130px] h-[30px] bg-[rgba(244,79,1,1)] rotate-[-17.14deg] -right-24 bottom-[25%] text-[10px] pt-[6px] text-center pl-2 rounded-[20px]">Software Development</div>
           
-          <h1 className='text-[120px] leading-[100%] font-bold'>IT Services <br/> That Elevates</h1>
+          <h1 className='text-[40px] lg:text-[120px] leading-[100%] font-bold'>IT Services <br className='hidden lg:block'/> That <br className='lg:hidden'/> Elevates</h1>
           <p className='text-[18px] mt-[10px] leading-[100%] text-[rgba(255,255,255,0.6)] lg:pr-44'>At OTB, we offer a unique and meticulous approach to web and app development, graphic design, custom solutions, software development and video editing</p>
         </div>
         <Image src='/hero_blur_right.svg' width={800} height={800} alt='background' className='absolute z-10 -bottom-1 right-0'/>
         <Image src='/hero_blur_left.svg' width={800} height={800} alt='background' className='absolute z-10 top-0 left-0'/>
-        <Image src='/abstract _design.svg' width={800} height={800} alt='background' className='absolute z-20 top-1/2 right-1/2 translate-x-1/2 translate-y-1/2'/>
+        <Image src='/abstract _design.svg' width={800} height={800} alt='background' className='absolute z-20 top-1/2 right-1/2 translate-x-1/2 translate-y-1/2 hidden lg:block'/>
+        <Image src='/abstract_design_mobile.svg' width={800} height={800} alt='background' className='absolute z-20 bottom-24 right-1/2 translate-x-1/2 translate-y-1/2 lg:hidden'/>
     </header>
   )
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='w-full flex justify-between rounded-[10px] items-center bg-[rgb(255,255,255,0.06)] py-8 px-8'>
-        <Image src='/otb_logo.svg' width={90} height={50} alt='OTB'/>
+    <nav className='w-full flex justify-between rounded-[10px] items-center bg-[rgb(255,255,255,0.06)] px-4 py-2 lg:py-8 lg:px-8'>
+        <Image src='/otb_logo.svg' width={90} height={50} alt='OTB' className='hidden lg:block'/>
+        <Image src='/otb_logo.svg' width={40} height={50} alt='OTB' className='lg:hidden'/>
         <div className="flex items-center gap-[33px]">
-            <button className='primary-btn text-center rounded-[40px] h-[50px] w-[166px]'>Get in touch</button>
+            <button className='primary-btn text-center rounded-[20px] lg:rounded-[40px] h-[40px] lg:h-[50px] px-4 lg:w-[166px]'>Get in touch</button>
             <div className="w-[50px] h-[50px] rounded-[50%] bg-[rgba(255,255,255,0.1)] flex flex-col gap-3 justify-center items-center ">
                 <Image src='/hambuger_menu.svg' width={40} height={40} alt='menu'/>
                 <Image src='/hambuger_menu.svg' width={40} height={40} alt='menu'/>
