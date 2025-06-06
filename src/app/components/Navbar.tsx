3bc58e1e994d541b254e353fdc46a79aaae2800f
rajiss-ctrl <omosanjos77@gmail.com>
2025-06-06 00:45:26 -0700

    Implement menu responsiveness

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -36,13 +36,13 @@ const Navbar = () => {
             className='lg:hidden cursor-pointer z-50'
           />
         </Link>
-        <div className="flex items-center gap-[33px]">
+        <div className="flex items-center gap-2 lg:gap-[33px]">
             <button className='primary-btn cursor-pointer text-center rounded-[20px] lg:rounded-[40px] h-[40px] lg:h-[50px] px-4 lg:w-[166px]'>
               Get in touch
             </button>
             <div 
               onClick={handleToggle} 
-              className="cursor-pointer w-[50px] h-[50px] rounded-[50%] bg-[rgba(255,255,255,0.1)] flex flex-col gap-3 justify-center items-center"
+              className="hidden lg:flex cursor-pointer w-[50px] h-[50px] rounded-[50%] bg-[rgba(255,255,255,0.1)]  flex-col gap-3 justify-center items-center"
             >
                 <Image 
                   src='/hambuger_menu.svg' 
@@ -62,6 +62,32 @@ const Navbar = () => {
                   src='/menu_cancel.png' 
                   width={40} 
                   height={40} 
+                  alt='cancel' 
+                  className={`${open ? 'block p-2' : 'hidden'}`}
+                />
+            </div>
+            <div 
+              onClick={handleToggle} 
+              className="cursor-pointer w-[50px] h-[50px] rounded-[50%] bg-[rgba(255,255,255,0.1)] lg:hidden  flex flex-col gap-3 justify-center items-center"
+            >
+                <Image 
+                  src='/hambuger_menu.svg' 
+                  width={30} 
+                  height={30} 
+                  alt='menu' 
+                  className={`${!open ? 'block' : 'hidden'}`}
+                />
+                <Image 
+                  src='/hambuger_menu.svg' 
+                  width={30} 
+                  height={30} 
+                  alt='menu' 
+                  className={`${!open ? 'block' : 'hidden'}`}
+                />
+                <Image 
+                  src='/menu_cancel.png' 
+                  width={30} 
+                  height={30} 
                   alt='cancel' 
                   className={`${open ? 'block p-2' : 'hidden'}`}
                 />
