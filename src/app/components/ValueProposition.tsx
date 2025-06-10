7d6a675031638ab8743a49eac93f668f6d6c3809
rajiss-ctrl <omosanjos77@gmail.com>
2025-06-10 16:07:21 -0700

    padding adjustment

--- src/app/components/ValueProposition.tsx
+++ src/app/components/ValueProposition.tsx
@@ -7,7 +7,7 @@ const ValueProposition = () => {
         <h1 className='font-bricolage my-[80px] text-[32px] lg:text-[60px] leading-[100%] font-bold'>Why Choose Us?</h1>
         <div className="">
             <div className="flex flex-col lg:flex-row items-center border-t lg:border-b border-gray-900">
-                <div className="lg:border-r lg:border-gray-900 p-10">
+                <div className="lg:border-r lg:border-gray-900 lg:w-1/2 p-10">
                     <div className="flex items-center gap-4">
                         <div className="w-[50px] h-[50px] rounded-[10px] p-2 bg-[linear-gradient(to_bottom,_#3A44FF80,_#24242400)]">
                             <Image src="/award.svg" width={40} height={40} alt="award icon"/>
@@ -16,7 +16,7 @@ const ValueProposition = () => {
                     </div>
                     <p className='font-inter lg:text-[20px] text-[rgba(230,230,230,0.7)] font-[400] leading-[100%] mt-6'>With years of experience in the field, we bring a wealth of industry knowledge and expertise to every project.</p>
                 </div>
-                <div className=" p-10">
+                <div className=" lg:w-1/2 p-10">
                     <div className="flex items-center gap-4">
                         <div className="w-[50px] h-[50px] rounded-[10px] p-2 bg-[linear-gradient(to_bottom,_#3A44FF80,_#24242400)]">
                             <Image src="/crown.svg" width={40} height={40} alt="award icon"/>
@@ -27,7 +27,7 @@ const ValueProposition = () => {
                 </div>
             </div>
             <div className="flex flex-col lg:flex-row items-center">
-                <div className="lg:border-r border-gray-900 p-10">
+                <div className="lg:border-r border-gray-900 lg:w-1/2 p-10">
                     <div className="flex items-center gap-4">
                         <div className="w-[50px] h-[50px] rounded-[10px] p-2 bg-[linear-gradient(to_bottom,_#3A44FF80,_#24242400)]">
                             <Image src="/badge.svg" width={40} height={40} alt="award icon"/>
@@ -36,7 +36,7 @@ const ValueProposition = () => {
                     </div>
                     <p className='font-inter lg:text-[20px] text-[rgba(230,230,230,0.7)] font-[400] leading-[100%] mt-6'>We pay meticulous attention to every aspect of our work, ensuring high-quality deliverables that meet and exceed industry standards.</p>
                 </div>
-                <div className=" p-10">
+                <div className=" p-10 lg:w-1/2">
                     <div className="flex items-center gap-4">
                         <div className="w-[50px] h-[50px] rounded-[10px] p-2 bg-[linear-gradient(to_bottom,_#3A44FF80,_#24242400)]">
                             <Image src="/community.svg" width={40} height={40} alt="award icon"/>
